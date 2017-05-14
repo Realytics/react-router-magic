2d07a759a207b4ca492ed4c15fcd432ef1c1395c
Etienne Dldc <etienne.dldc@outlook.fr>
2017-05-14 20:03:43 +0200

    Add matchProps to Redirect

--- src/Redirect.tsx
+++ src/Redirect.tsx
@@ -3,12 +3,13 @@ import { Component, ValidationMap } from 'react';
 import * as PropTypes from 'prop-types';
 import { Path, LocationDescriptorObject, History } from 'history';
 import { Store } from './Store';
-import { MatchResult, match, createLocationDescriptorObject, RealtiveAbsoluteProps } from './utils';
+import { MatchResult, MatchProps, match, createLocationDescriptorObject, RealtiveAbsoluteProps } from './utils';
 import { RouterStoreState } from './RouterProvider';
 
 export namespace RedirectTypes {
 
   export type Props = (
+    MatchProps &
     RealtiveAbsoluteProps & {
       to: Path | LocationDescriptorObject;
       push?: boolean;
@@ -36,16 +37,23 @@ export class Redirect extends Component<RedirectTypes.Props, {}> {
     const history: History = this.context.router.history;
     const push: boolean = this.props.push === true;
 
-    const to: LocationDescriptorObject = createLocationDescriptorObject(
-      this.props.to,
-      this.props,
-      this.context.routerStore.getState().match
+    const matchResult: MatchResult = match(
+      this.context.routerStore,
+      this.props
     );
 
-    if (push) {
-      history.push(to);
-    } else {
-      history.replace(to);
+    if (matchResult.match) { // Redirect
+      const to: LocationDescriptorObject = createLocationDescriptorObject(
+        this.props.to,
+        this.props,
+        this.context.routerStore.getState().match
+      );
+
+      if (push) {
+        history.push(to);
+      } else {
+        history.replace(to);
+      }
     }
 
   }
