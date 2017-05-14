3cd489d2c02a8ac910d9e89a266ed616e5eecdcf
Etienne Dldc <etienne.dldc@outlook.fr>
2017-05-14 18:38:22 +0200

    Add export for redirect

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Route } from './Route';
 export { Switch } from './Switch';
 export { NavProvider } from './NavProvider';
-export { RouterProvider } from './RouterProvider';
+export { RouterProvider, RouterProviderTypes } from './RouterProvider';
 export { matchPath } from './matchPath';
+export { Redirect } from './Redirect';
