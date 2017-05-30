6dbfe801b47b49f3e50c312b3c8ba12aa601bfe5
Etienne Dldc <etienne.dldc@outlook.fr>
2017-05-30 11:40:49 +0200

    Reduce build size

--- src/RouterProvider.tsx
+++ src/RouterProvider.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import isEqual = require('lodash/isEqual');
+import isEqual = require('deep-equal');
 import { Component, ValidationMap } from 'react';
 import { History, Location } from 'history';
 import { Store } from './Store';
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export { Switch } from './Switch';
 export { NavProvider } from './NavProvider';
 export { RouterProvider, RouterProviderTypes } from './RouterProvider';
 export { Redirect } from './Redirect';
-export { PathPattern } from './PathPattern';
